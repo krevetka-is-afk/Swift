654cd04ddd31988c91a3ced1867e7775c2cf04ac
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2024-11-05 22:42:01 +0300

    sem 4

--- seminar4/seminar4/ViewController.swift
+++ seminar4/seminar4/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let jsonDecoder = JSONDecoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var worker = BaseURLWorker(baseURL: "https://www.flickr.com")
+        var requet = Request(endpoint: FlickrEndpoint.feeds)
+        
+        worker.execute(with: requet) { response in
+            switch response {
+            case .success(let serverResponse):
+                print(serverResponse)
+//                jsonDecoder.    
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
 }
 
+enum FlickrEndpoint: Endpoint {
+    case feeds
+
+    var compositePath: String {
+        switch self {
+        case .feeds:
+            return "/services/feeds/photos_public.gne"
+        }
+    }
+    
+    var headers: [String: String] {
+        return [:]
+    }
+    
+    var parameters: [String : String]? {
+        switch self {
+        case .feeds:
+            return ["tags": "soccer", "format": "json"]
+        }
+    }
+    
+}
+
+// https://www.flickr.com/services/feeds/photos_public.gne?tags=soccer&format=json
